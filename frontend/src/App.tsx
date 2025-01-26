c3be07e09b8164156f810746d20cabf0c436099f
julianhofmaninger <julian.hofmaninger@hsb-innovations.at>
2025-01-26 18:13:51 +0100

    Create frontend app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ErrorBoundary } from 'react-error-boundary';
+import './App.scss';
+import AuthProvider from './utils/context/AuthContext';
+import { Suspense } from 'react';
+import { LoadingScreen } from './components/default-components/loaders/loading-screen/loading-screen';
+import React from 'react';
+const Routes = React.lazy(() => import('./data/routes'));
+function MyFallbackComponent() {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>Failure</pre>
+      <button>Try again</button>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthProvider>
+      <ErrorBoundary FallbackComponent={MyFallbackComponent}>
+        <Suspense fallback={<LoadingScreen text='Denki Mini Projekt 2' />}>
+          <Routes />
+        </Suspense>
+      </ErrorBoundary>
+    </AuthProvider>
   )
 }
 
